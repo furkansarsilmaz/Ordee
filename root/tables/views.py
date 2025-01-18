b9e005f1b4c96dab822247fcfaa937e472338aea
furkansarsilmaz <160139624+furkansarsilmaz@users.noreply.github.com>
2025-01-18 22:07:16 +0300

    minor changes
    minor change

--- root/tables/views.py
+++ root/tables/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-
+@login_required(login_url="base.html")
 def tables(request):
+    print(request.user.is_authenticated)
     return render(request,'tables/tables.html')
