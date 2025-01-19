6faada64ea974f2c14581ad3c735f53edfb708a4
furkansarsilmaz <160139624+furkansarsilmaz@users.noreply.github.com>
2025-01-19 18:33:12 +0300

    working

--- root/tables/urls.py
+++ root/tables/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.tables,name='tables')
+    path('',views.tables,name='tables'),
+    path('menu',views.menu,name='menu')
 ]
--- root/tables/views.py
+++ root/tables/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Menu
+
 # Create your views here.
 @login_required(login_url="base.html")
 def tables(request):
     print(request.user.is_authenticated)
     return render(request,'tables/tables.html')
+
+def menu(request):
+    products = Menu.objects.all()
+    return render(request,'tables/menu.html',{'products' : products})
